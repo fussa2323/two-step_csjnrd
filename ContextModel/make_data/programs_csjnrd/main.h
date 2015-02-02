f1dead05cf9d85cf0407196231ee279a5839cbaf
Yohei Arikilab <yoohei2323@gmail.com>
2015-02-02 19:15:56 +0900

    csjnrd-versionのContxt-score計算用プログラムの追加

---
+++ ContextModel/make_data/programs_csjnrd/main.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <list>
+#include <vector>
+#include <math.h>
+#include <map>
+#include <cstring>
+
+
+#include "cn_elem.h"
+#include "confusion_set.h"
+#include "confusion_network.h"
+
+std::vector<std::string> split(std::string str, std::string delim);
+ std::string exec(char*);
